a2e0331ac81203eeaebad8059621ab5eac79e082
saathathm <saaadhath@gmail.com>
2025-10-26 12:05:28 +0530

    Removed: Unwanted Routes in Web

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,3 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/users', [UserController::class, 'index']);
-
-// Route::get('/tasks', [TaskController::class, 'index']);
-// Route::post('/tasks', [TaskController::class, 'store']);
